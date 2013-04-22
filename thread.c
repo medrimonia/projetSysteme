bdfc00ebead8e857303f4687d793b3c9d2d12285
Ludovic Hofer <medrimonia@gmail.com>
2013-04-22 11:06:38 +0200

    Adding implementation comments and removing exit_context (not used anymore)

--- thread.c
+++ thread.c
@@ -13,20 +13,23 @@ void initialize_thread_handler();
 
 struct thread{
   ucontext_t context;
+  /* Stack_id is used in order to work properly with valgrind */
   int stack_id;
+  /* Status is needed in order to allow join to work properly */
   int status;
+  /* retval is used in join and allow to store the return value */
   void * retval;
 };
 
-/* L'identifiant du thread est mis à jour au fur et à mesure
+/* current_thread is updated each time before swapping of thread. It will
+ * be replaced by a pointer when lists will be used
  */
 int current_thread = 0;
+/* This array of threads will be repaced by a list */
 struct thread * threads = NULL;
 int nb_threads = 0;
 int nb_threads_waiting_join = 0;
 int next_thread_create = 0;
-ucontext_t exit_context;
-int exit_context_stack_id;
 
 struct thread * add_thread(){
   threads[next_thread_create].status = 0;
@@ -49,13 +52,18 @@ struct thread * next_thread(){
     }
   }while(1);
 }
-
+/* The wrapper function ensure that the return value will be saved
+ * and that the thread will call thread_exit before dying
+ */
 void wrapper(void *(*func)(void *), void * funcarg){
   struct thread * this_thread = thread_self();
   void * retval = func(funcarg);
   thread_exit(retval);
 }
 
+/* Return the address of the current thread, if the thread handler hadn't
+ * been initialized, it starts it and return a valid address
+ */
 thread_t thread_self(){
   if (next_thread_create == 0){
     initialize_thread_handler();
@@ -63,23 +71,16 @@ thread_t thread_self(){
   return &threads[current_thread];
 }
 
+/* Allocate memory and initialize everything needed for thread handling
+ */
 void initialize_thread_handler(){
   threads = malloc(NB_THREADS_MAX * sizeof(struct thread));
   struct thread * this_thread = add_thread();
-  getcontext(&exit_context);
-  exit_context.uc_stack.ss_size = STACK_SIZE;
-  exit_context.uc_stack.ss_sp = malloc(exit_context.uc_stack.ss_size);
-  exit_context_stack_id =
-    VALGRIND_STACK_REGISTER(exit_context.uc_stack.ss_sp,
-                            exit_context.uc_stack.ss_sp
-                            + exit_context.uc_stack.ss_size);
-  exit_context.uc_link = NULL;
-  makecontext(&exit_context,(void (*)(void)) thread_exit, 1, 0);
 }
 
+/* Free all the memory used for thread handling
+ */
 void end_thread_handling(){
-  VALGRIND_STACK_DEREGISTER(exit_context_stack_id);
-  free(exit_context.uc_stack.ss_sp);
   free(threads);
   nb_threads = 0;
   current_thread = 0;
@@ -87,6 +88,11 @@ void end_thread_handling(){
   next_thread_create = 0;
 }
 
+/* Create a new thread (initializing the thread handler if necessary)
+ * allocate the necessary memory for the new thread stack and then launch
+ * a wrapper context which will execute the specified function with the
+ * arguments specified.
+ */
 int thread_create(thread_t * newthread,
                   void *(*func)(void *),
                   void * funcarg){
@@ -108,6 +114,9 @@ int thread_create(thread_t * newthread,
   return 0;
 }
 
+/* If other threads are running at the same time, swap context to the next
+ * one, otherwise it simply returns
+ */
 int thread_yield(){
   if (next_thread_create == 0){
     return 0;
@@ -120,6 +129,13 @@ int thread_yield(){
   return 0;
 }
 
+/* Wait until a certain thread has finished it's job and put the value
+ * in retval.
+ * If after the join, there's only one thread remaining and no threads
+ * waiting to be joined, all the resources used by thread handling are freed.
+ * This choice might results with the trouble that an address obtained by a
+ * call to thread_self() might not be valid anymore.
+ */
 int thread_join(thread_t thread, void ** retval){
   struct thread * to_wait = (struct thread *) thread;
   while(to_wait->status != STATUS_TERMINATED){
@@ -129,16 +145,20 @@ int thread_join(thread_t thread, void ** retval){
   if (retval != NULL)
     *retval = to_wait->retval;
   free(to_wait->context.uc_stack.ss_sp);
+  VALGRIND_STACK_DEREGISTER(to_wait->stack_id);
   if (nb_threads == 1 && nb_threads_waiting_join == 0)
     end_thread_handling();
   return 0;
 }
 
+/* Close the thread currently active, place the appropriate value in retval
+ * and let another thread continue it's execution.
+ * This function doesn't free the resources used by the stack
+ */
 void thread_exit(void *retval){
   struct thread * my_thread = thread_self();
   my_thread->status = STATUS_TERMINATED;
   my_thread->retval = retval;
-  VALGRIND_STACK_DEREGISTER(my_thread->stack_id);
   if (nb_threads == 1)
     exit(EXIT_SUCCESS);
   struct thread * next = next_thread();
