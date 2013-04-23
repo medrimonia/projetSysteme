d1e75946bad974ed1a67dd7830efacda34758f35
tsanch3z <tsanchez@enseirb-matmeca.fr>
2013-04-23 11:56:02 +0200

    avancée du travail sur les liste + Makefile pour un exemple

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 thread.o: thread.c
 	cc `pkg-config --cflags --libs glib-2.0` -Wall -Wextra thread.c -c
+
+example: thread.o example.c
+	cc `pkg-config --cflags --libs glib-2.0` -Wall -Wextra thread.o example.c -o example
--- thread.c
+++ thread.c
@@ -6,7 +6,6 @@
 #include <ucontext.h>
 #include <valgrind/valgrind.h>
 
-#define NB_THREADS_MAX 100
 #define STACK_SIZE 64 * 1024
 #define STATUS_TERMINATED 1
 
@@ -19,12 +18,11 @@ struct thread{
   void * retval;
 };
 
-GList *threadss = NULL;
+GList *threads = NULL;
 
 /* L'identifiant du thread est mis à jour au fur et à mesure
  */
 int current_thread = 0;
-struct thread * threads = NULL;
 int nb_threads = 0;
 int nb_threads_waiting_join = 0;
 int next_thread_create = 0;
@@ -32,51 +30,57 @@ ucontext_t exit_context;
 int exit_context_stack_id;
 
 struct thread * add_thread(){
-  threads[next_thread_create].status = 0;
-  threads[next_thread_create].retval = NULL;
-  threads[next_thread_create].context.uc_link = NULL;
-  threadss = g_list_append(threadss, &threads[next_thread_create]);
+  struct thread *to_add = malloc(sizeof(struct thread));
+  to_add->status = 0;
+  to_add->retval = NULL;
+  to_add->context.uc_link = NULL;
+  threads = g_list_append(threads, &to_add);
   nb_threads++;
-  return &threads[next_thread_create++];
+  next_thread_create++;
+  return to_add;
 }
 
 // switch to next thread
 struct thread * next_thread(){
   int next = current_thread;
-  do{
-    next++;
-    if (next == next_thread_create)
-      next = 0;
-    if (threads[next].status != STATUS_TERMINATED){
-      current_thread = next;
-      return &threads[next];
+  struct thread *next_running, *running;
+  running = g_list_nth_data(threads, next);
+  next_running = g_list_nth_data(threads, ++next);
+  while (running != next_running) {
+    if(running == NULL) {
+      next_running = (struct thread*) g_list_first(threads);
     }
-  }while(1);
+    else if ((next_running = (struct thread*) g_list_next(threads)) == NULL) {
+      next_running = (struct thread*) g_list_first(threads);
+    }
+    if(next_running->status != STATUS_TERMINATED) {
+      current_thread = g_list_index(threads, next_running);
+      return next_running;
+    }
+  }
+  return NULL;
 }
 
 void wrapper(void *(*func)(void *), void * funcarg){
-  struct thread * this_thread = thread_self();
   void * retval = func(funcarg);
   thread_exit(retval);
 }
-
 thread_t thread_self(){
   if (next_thread_create == 0){
     initialize_thread_handler();
   }
-  return &threads[current_thread];
+  return g_list_nth_data(threads, current_thread);
 }
 
 void initialize_thread_handler(){
-  threads = malloc(NB_THREADS_MAX * sizeof(struct thread));
   struct thread * this_thread = add_thread();
   getcontext(&exit_context);
   exit_context.uc_stack.ss_size = STACK_SIZE;
   exit_context.uc_stack.ss_sp = malloc(exit_context.uc_stack.ss_size);
   exit_context_stack_id =
     VALGRIND_STACK_REGISTER(exit_context.uc_stack.ss_sp,
-                            exit_context.uc_stack.ss_sp
-                            + exit_context.uc_stack.ss_size);
+        exit_context.uc_stack.ss_sp
+        + exit_context.uc_stack.ss_size);
   exit_context.uc_link = NULL;
   makecontext(&exit_context,(void (*)(void)) thread_exit, 1, 0);
 }
@@ -84,7 +88,7 @@ void initialize_thread_handler(){
 void end_thread_handling(){
   VALGRIND_STACK_DEREGISTER(exit_context_stack_id);
   free(exit_context.uc_stack.ss_sp);
-  free(threads);
+  g_list_free(threads);
   nb_threads = 0;
   current_thread = 0;
   threads = NULL;
@@ -92,8 +96,8 @@ void end_thread_handling(){
 }
 
 int thread_create(thread_t * newthread,
-                  void *(*func)(void *),
-                  void * funcarg){
+    void *(*func)(void *),
+    void * funcarg){
   if (next_thread_create == 0){
     initialize_thread_handler();
   }
@@ -104,8 +108,8 @@ int thread_create(thread_t * newthread,
   new_context->uc_stack.ss_sp = malloc(new_context->uc_stack.ss_size);
   new_thread->stack_id =
     VALGRIND_STACK_REGISTER(new_context->uc_stack.ss_sp,
-                            new_context->uc_stack.ss_sp
-                            + new_context->uc_stack.ss_size);
+        new_context->uc_stack.ss_sp
+        + new_context->uc_stack.ss_size);
   new_context->uc_link = NULL;
   makecontext(new_context, (void (*)(void)) wrapper, 2, func, funcarg);
   *newthread = (void *)new_thread;
